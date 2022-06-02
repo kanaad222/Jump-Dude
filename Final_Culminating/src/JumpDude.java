8130f893901baa2dd8b6439070487127ed4decf7
Dlion516 <105813538+Dlion516@users.noreply.github.com>
2022-06-02 13:31:17 -0400

    Update JumpDude.java

--- Final_Culminating/src/JumpDude.java
+++ Final_Culminating/src/JumpDude.java
@@ -16,16 +16,19 @@ public class JumpDude extends JPanel implements Runnable, KeyListener{
 	// Images
 	public static BufferedImage menu;
 	public static BufferedImage player;
+	public static BufferedImage player2;
+	public static BufferedImage player3;
+	public static BufferedImage player4;
 	public static BufferedImage jumpIndicator;
 	
 	// Player Stats
-	public static int playerX = 50;
-	public static int playerY = 500;
-	public static int height = 500;
+	public static int playerX = 100;
+	public static int playerY = 560;
+	public static int height = 560;
 	public static boolean grounded = true;
 	public static int fallingSpeed = 0;
 	
-	// Levels (Placeholders)
+	// Levels (Place holders)
 	public static BufferedImage level1;
 	public static BufferedImage level2;
 	public static BufferedImage Level3;
@@ -48,9 +51,10 @@ public class JumpDude extends JPanel implements Runnable, KeyListener{
 		try {
 			// Initialize Variables
 			menu = ImageIO.read(new File("menu.png"));
-			player = ImageIO.read(new File("testPlayer.png"));
-			jumpIndicator = ImageIO.read(new File("jumpIndicator2.png"));
 			level1 = ImageIO.read(new File("S1.1.jpg"));
+			player = ImageIO.read(new File("Character Sprite1.png"));
+			player2 = ImageIO.read(new File("Character Sprite2.png"));
+			jumpIndicator = ImageIO.read(new File("jumpIndicator1.png"));
 		}
 		catch(Exception e) {
 			System.out.println("IMAGE NOT FOUND");
@@ -77,7 +81,7 @@ public class JumpDude extends JPanel implements Runnable, KeyListener{
 			playerY += fallingSpeed;
 			fallingSpeed += 1;
 			if(playerY >= height) {
-				playerY = playerY;  
+				playerY = height;
 				grounded = true;
 				fallingSpeed = 0;
 			}
@@ -97,9 +101,15 @@ public class JumpDude extends JPanel implements Runnable, KeyListener{
 		// Level 1
 		if(gameState == 1) {
 			g.drawImage(level1, 0, 0, null);
-			g.drawImage(player, playerX, playerY, null);
+			if(grounded) {
+				g.drawImage(player, playerX, playerY, null);
+			}
+			
 			g.drawLine(1, 400, 50, 400);
 			g.drawImage(jumpIndicator, 0, 0, null);
+			if(!  grounded) {
+				g.drawImage(player2, playerX, playerY, null);
+			}
 			
 		}
 		
@@ -147,10 +157,10 @@ public class JumpDude extends JPanel implements Runnable, KeyListener{
 				height = 500;
 			}
 			if(e.getKeyCode() == 50) {
-				height = 600;
+				height -= 100;
 			}
 			if(e.getKeyCode() == 51) {
-				height = 700;
+				height -= 200;
 			}
 		}
 		
@@ -175,3 +185,4 @@ public class JumpDude extends JPanel implements Runnable, KeyListener{
 		return Math.atan2(y2-y1, x2-x1) * 180 / Math.PI;
 	}
 }
+// https://medium.com/@brazmogu/physics-for-game-dev-a-platformer-physics-cheatsheet-f34b09064558
